3bc3f52da5a6bd197fb0508f32c5433668208453
Krishnaarunangsu <arunangsutech@gmail.com>
2019-07-20 21:05:11 +0530

    11th Commit

--- data_exploration/python_abzooba_client_1.py
+++ data_exploration/python_abzooba_client_1.py
@@ -7,7 +7,7 @@ print(check_output("ls -l", shell=True).decode())
 #print(check_output("echo $PYTHONPATH", shell=True).decode())
 pwd='abz00ba1nc'
 cmd='docker ps'
-print(check_output("echo {} | sudo -S {}'.format(pwd, cmd)", shell=True).decode())
+print(check_output("echo {} | sudo -S {}".format(pwd, cmd), shell=True).decode())
 
 
 
