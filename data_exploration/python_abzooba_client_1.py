5b263e7d7a12e737821e162c3c7cfa888199a0b0
Krishnaarunangsu <arunangsutech@gmail.com>
2019-07-20 21:21:32 +0530

    13th Commit

--- data_exploration/python_abzooba_client_1.py
+++ data_exploration/python_abzooba_client_1.py
@@ -1,17 +1,23 @@
-print('Abzooba')
+print('Xpresso AI-Abzooba')
+print('***************************************')
 from subprocess import check_output
 
 
 
 #print(check_output("ls -l", shell=True).decode())
 #print(check_output("echo $PYTHONPATH", shell=True).decode())
+print('Docker Installation Check')
+print('***************************************')
 pwd='abz00ba1nc'
 cmd='docker ps'
 print(check_output("echo {} | sudo -S {}".format(pwd, cmd), shell=True).decode())
 
+print('Kubernetes Installation Check')
+print('***************************************')
 cmd_1 = 'kubectl get services'
 #print(check_output("kubectl explain pods,svc",shell=True).decode())
 print(check_output("echo {} | sudo -S {}".format(pwd, cmd_1), shell=True).decode())
+print('*****************************************************************************')
 
 
 
