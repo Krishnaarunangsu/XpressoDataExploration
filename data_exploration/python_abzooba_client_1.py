c4cb809d98ddee2e10f57c7566a1e53df860fbe2
Krishnaarunangsu <arunangsutech@gmail.com>
2019-07-20 21:17:03 +0530

    12th Commit

--- data_exploration/python_abzooba_client_1.py
+++ data_exploration/python_abzooba_client_1.py
@@ -3,11 +3,15 @@ from subprocess import check_output
 
 
 
-print(check_output("ls -l", shell=True).decode())
+#print(check_output("ls -l", shell=True).decode())
 #print(check_output("echo $PYTHONPATH", shell=True).decode())
 pwd='abz00ba1nc'
 cmd='docker ps'
 print(check_output("echo {} | sudo -S {}".format(pwd, cmd), shell=True).decode())
+
+cmd_1 = 'kubectl get services'
+#print(check_output("kubectl explain pods,svc",shell=True).decode())
+print(check_output("echo {} | sudo -S {}".format(pwd, cmd_1), shell=True).decode())
 
 
 
