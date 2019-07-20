0a47aed703f0a06e8890ac9a381fa37e0ee87092
Krishnaarunangsu <arunangsutech@gmail.com>
2019-07-20 21:02:18 +0530

    10th Commit

--- data_exploration/python_abzooba_client_1.py
+++ data_exploration/python_abzooba_client_1.py
@@ -1,6 +1,13 @@
 print('Abzooba')
-import subprocess
-
 from subprocess import check_output
+
+
+
 print(check_output("ls -l", shell=True).decode())
-print(check_output("echo $PYTHONPATH", shell=True).decode())
+#print(check_output("echo $PYTHONPATH", shell=True).decode())
+pwd='abz00ba1nc'
+cmd='docker ps'
+print(check_output("echo {} | sudo -S {}'.format(pwd, cmd)", shell=True).decode())
+
+
+
