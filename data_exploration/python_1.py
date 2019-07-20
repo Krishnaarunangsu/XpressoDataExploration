469a98940a0aaae1ad02433759a646057a24401e
Krishnaarunangsu <arunangsutech@gmail.com>
2019-07-20 17:19:42 +0530

    6th Commit

--- data_exploration/python_1.py
+++ data_exploration/python_1.py
@@ -2,9 +2,11 @@
 import subprocess
 
 print('Abzooba')
-subprocess.call(["ls", "-l"])
+#subprocess.call(["ls", "-l"])
 
 # p = subprocess.Popen(['docker', 'ps', '|', 'grep', 'myimagename'], stdout=subprocess.PIPE)
-p = subprocess.Popen(['sudo','docker', 'ps'], stdout=subprocess.PIPE)
-print(p.stdout)
+#p = subprocess.Popen(['sudo','docker', 'ps'], stdout=subprocess.PIPE)
+#print(p.stdout)
+
+subprocess.call(['docker', 'ps'])
 
