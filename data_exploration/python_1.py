f82733b0647f58a49291ea2069dae455a1d5bb28
Krishnaarunangsu <arunangsutech@gmail.com>
2019-07-20 16:43:04 +0530

    5th commit

--- data_exploration/python_1.py
+++ data_exploration/python_1.py
@@ -6,5 +6,5 @@ subprocess.call(["ls", "-l"])
 
 # p = subprocess.Popen(['docker', 'ps', '|', 'grep', 'myimagename'], stdout=subprocess.PIPE)
 p = subprocess.Popen(['sudo','docker', 'ps'], stdout=subprocess.PIPE)
-#print(p.stdout)
+print(p.stdout)
 
